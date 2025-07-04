c8788d535e5abd429afd126346e51a165fc83681
Amira Abdurahman <amiraabdurahman8@gmail.com>
2025-07-05 00:05:55 +0300

    working site

--- my-portfolio/my-portfolio-vite/src/components/Navbar.jsx
+++ my-portfolio/my-portfolio-vite/src/components/Navbar.jsx
@@ -1,31 +1,17 @@
-import React from 'react';
-import ThemeToggle from './ThemeToggle';
-import './Navbar.css';
+// src/components/Navbar.jsx
+import React from "react";
+import "./Navbar.css";
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="nav-left">
-        <a href="#hero" className="logo">Amira</a>
-      </div>
-
-      <div className="nav-center">
+      <div className="logo">Amira</div>
+      <div className="nav-links">
+        <a href="#hero">Home</a>
         <a href="#about">About</a>
         <a href="#projects">Projects</a>
         <a href="#certificates">Certificates</a>
         <a href="#contact">Contact</a>
-      </div>
-
-      <div className="nav-right">
-        <a
-          href="/resume.pdf"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="resume-button"
-        >
-          Resume
-        </a>
-        <ThemeToggle />
       </div>
     </nav>
   );
