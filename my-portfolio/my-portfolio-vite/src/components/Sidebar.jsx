04809033a109b7d70a90546976c4eed058eb03df
Amira Abdurahman <amiraabdurahman8@gmail.com>
2025-07-05 00:46:19 +0300

    essential sections are added

--- my-portfolio/my-portfolio-vite/src/components/Sidebar.jsx
+++ my-portfolio/my-portfolio-vite/src/components/Sidebar.jsx
@@ -1,6 +1,14 @@
 // src/components/Sidebar.jsx
 import React from 'react';
-import { FaHome, FaUser, FaProjectDiagram, FaEnvelope } from 'react-icons/fa';
+import {
+  FaHome,
+  FaUser,
+  FaCode,
+  FaProjectDiagram,
+  FaBriefcase,
+  FaGraduationCap,
+  FaAward,
+} from 'react-icons/fa';
 import './Sidebar.css';
 
 function Sidebar() {
@@ -12,11 +20,20 @@ function Sidebar() {
       <a href="#about" title="About">
         <FaUser />
       </a>
+      <a href="#skills" title="Skills">
+        <FaCode />
+      </a>
       <a href="#projects" title="Projects">
         <FaProjectDiagram />
       </a>
-      <a href="#contact" title="Contact">
-        <FaEnvelope />
+      <a href="#experience" title="Experience">
+        <FaBriefcase />
+      </a>
+      <a href="#education" title="Education">
+        <FaGraduationCap />
+      </a>
+      <a href="#certificates" title="Certificates">
+        <FaAward />
       </a>
     </div>
   );
